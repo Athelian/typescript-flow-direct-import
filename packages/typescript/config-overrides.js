7c7e2574c21c3a9c3fd311cbcda60701595490b1
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-10-18 22:28:21 +0900

    configure out of cra root loading

---
+++ packages/typescript/config-overrides.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = function override(config, env) {
+  const babelRule = config.module.rules[1].oneOf.find(
+    (rules) => rules.include === path.resolve(__dirname, "./src")
+  );
+  babelRule.include = [
+    babelRule.include,
+    path.resolve(__dirname, "../flow/src")
+  ];
+  return config;
+};
