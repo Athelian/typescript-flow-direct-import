39ab54c327f1b07275b3291391f39d37cd76404e
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-10-29 22:15:12 +0900

    ts decs in flow

---
+++ packages/typescript/src/components/Button.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import "./Button.css";
+declare type Props = {
+    text: string;
+};
+export declare const Button: ({ text }: Props) => JSX.Element;
+export default Button;
